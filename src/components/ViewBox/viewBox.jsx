811fe90c8e3f84ac74631f65f1efe77730e02c42
nicholasmelliott <30277860+nicholasmelliott@users.noreply.github.com>
2023-02-22 21:34:50 -0500

    Updated ViewBox component styles.

--- src/components/ViewBox/viewBox.jsx
+++ src/components/ViewBox/viewBox.jsx
@@ -59,13 +59,12 @@ const ViewBoxWrapper = (props) => {
 
   const canvasDim = rectangles.reduce((acc, r) => {
     acc.width += r.width * multiplier + cBorder;
-    acc.height += r.height * multiplier + cBorder;
+    acc.height += r.height * multiplier + cMargin;
     return acc;
   }, { width: 0, height: 0 });
   
-  const maxWidth = Math.max(...rectangles.map(o => o.width)) * multiplier + cBorder;
-  const viewboxWidth = Math.max(canvasDim.width, maxWidth);
-  const viewboxHeight = canvasDim.height;
+  const viewboxWidth = Math.max(...rectangles.map(o => o.width)) * multiplier + (cBorder * 2);
+  const viewboxHeight = canvasDim.height + cBorder;
 
   return (
     <div>
@@ -78,33 +77,34 @@ const ViewBoxWrapper = (props) => {
             y={cMargin + prevHeight}
             width={r.width * multiplier}
             height={r.height * multiplier}
-            fill="#82FFFF"
+            fill="#a1c5ff"
             stroke="black"
           />
           {/* Draw donor piece dimensions next to corresponding sides */}
           <text 
             x={cMargin + (r.width * multiplier) / 2} 
             y={cMargin - 10 + prevHeight} 
-            textAnchor="middle" fontSize="12" 
+            textAnchor="middle" 
+            fontSize="25" 
             fill="#000000"
           >
             {r.width}
           </text>
           <text 
-            x={cMargin - 15} 
+            x={cMargin - 25} 
             y={cMargin + (r.height * multiplier) / 2 + prevHeight} 
             textAnchor="middle" 
-            fontSize="12" 
+            fontSize="25" 
             fill="#000000"
           >
             {r.height}
           </text>
           <text 
-            x={cMargin + (r.width * multiplier) + 50} 
+            x={(cMargin * 2) + (r.width * multiplier)} 
             y={cMargin + (r.height * multiplier) / 2 + prevHeight} 
             textAnchor="middle" 
-            fontSize="22" 
-            fill="#00C800"
+            fontSize="30" 
+            fill="#000"
           >
             ${r.price}
           </text>
@@ -117,14 +117,15 @@ const ViewBoxWrapper = (props) => {
                 y={b.y * multiplier + cMargin + prevHeight}
                 width={b.width * multiplier}
                 height={b.height * multiplier}
-                fill={`rgb(0, 255, 255)`}
-                stroke="green"
+                fill="#c7dcff"
+                strokeDasharray="10"
+                stroke="black"
               />
               {/* Draw the dimensions of the piece to-be-cut */}
               <text
                 x={(b.x * multiplier + cMargin) + (b.width * multiplier / 2)}
                 y={(b.y * multiplier + cMargin + prevHeight) + (b.height * multiplier / 2)}
-                stroke="green"
+                stroke="black"
                 textAnchor="middle"
                 fontSize={25}
               >
@@ -133,8 +134,8 @@ const ViewBoxWrapper = (props) => {
               {/* Draw the top dimension */}
               <text
                 x={(b.x * multiplier + cMargin) + (b.width * multiplier / 2)}
-                y={(b.y * multiplier + cMargin + 15 + prevHeight)}
-                fill="{`rgb(255, 255, 255)`}"
+                y={(b.y * multiplier + cMargin + 25 + prevHeight)}
+                fill="#fff"
                 textAnchor="middle"
                 fontSize={15}
               >
@@ -143,8 +144,8 @@ const ViewBoxWrapper = (props) => {
               {/* Draw the bottom dimension */}
               <text
                 x={(b.x * multiplier + cMargin) + (b.width * multiplier / 2)}
-                y={(b.y * multiplier + cMargin + b.height * multiplier - 5 + prevHeight)}
-                fill={`rgb(255, 255, 255)`}
+                y={(b.y * multiplier + cMargin + b.height * multiplier - 15 + prevHeight)}
+                fill="#fff"
                 textAnchor="middle"
                 fontSize={15}
               >
@@ -154,7 +155,7 @@ const ViewBoxWrapper = (props) => {
               <text
                 x={(b.x * multiplier + cMargin + 15)}
                 y={(b.y * multiplier + cMargin + (b.height * multiplier / 2) + prevHeight)}
-                fill={`rgb(255, 255, 255)`}
+                fill="#fff"
                 textAnchor="start"
                 fontSize={15}
               >
@@ -164,7 +165,7 @@ const ViewBoxWrapper = (props) => {
               <text
                 x={(b.x * multiplier + cMargin + b.width * multiplier - 15)}
                 y={(b.y * multiplier + cMargin + (b.height * multiplier / 2) + prevHeight)}
-                fill={`rgb(255, 255, 255)`}
+                fill="#fff"
                 textAnchor="end"
                 fontSize={15}
               >
@@ -177,7 +178,7 @@ const ViewBoxWrapper = (props) => {
           {priceTotal += r.price}
         </g>
       ))}
-      <text x={cMargin*2} y={cMargin + prevHeight} fill="#00C800" fontSize="42" textAnchor="middle">
+      <text x={cMargin*2} y={cMargin + prevHeight} fill="#000" fontSize="42" textAnchor="middle">
             ${parseFloat(priceTotal).toFixed(2)}
           </text>
       </svg>
