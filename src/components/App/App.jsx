8eecd79880e4197d90fa0fcd9b7d824bfba8f0eb
nicholasmelliott <30277860+nicholasmelliott@users.noreply.github.com>
2023-03-18 15:51:01 -0400

    Add dropup menu to conceal RespList Component and print button when not in use. Update ViewBox Component to span entire app height

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
-import './App.css'
+import { Button, Dropdown } from 'react-bootstrap';
+import './App.css';
 import ResponsiveList from '../RespList/respList';
 import ViewBox from '../ViewBox/viewBox';
 import print from '../../helpers/printViewBox';
@@ -31,12 +32,22 @@ function App() {
   return (
     <div className="App">
       <iframe id="ifmcontentstoprint" style={{"height": "0px", "width": "0px", "position": "absolute"}}></iframe>
-      <div style={{height: "60vh"}}>
+      <div>
         <ViewBox rectangles={rectangles} printRef={printRef} />
       </div>
-      <button  style={{"width": "100%"}} onClick={handlePrint}>Print</button>
-      <div style={{height: "40vh"}}>
-        <ResponsiveList rows={rows} setRows={setRows} rectangles={rectangles} setRectangles={setRectangles} />
+      <div style={{position: 'fixed', bottom: 0, left: 0, right: 0}}>
+        <div className="d-flex align-items-center justify-content-center w-100" style={{backgroundColor: '#eee', height: '60px', width: "100%"}}>
+          <Dropdown className="d-flex align-items-center justify-content-center w-100" autoClose={false} drop="up">
+            <Dropdown.Toggle variant="danger" id="dropdown-autoclose-false dropdown-button-drop-up">
+              <span>&#9776;</span>
+            </Dropdown.Toggle>
+            <Dropdown.Menu className="w-100 p-3 overflow-auto" style={{backgroundColor: '#eee'}}>
+              <Button variant="primary" style={{"width": "100%"}} onClick={handlePrint}>Print</Button>
+              <Dropdown.Divider />
+              <ResponsiveList rows={rows} setRows={setRows} rectangles={rectangles} setRectangles={setRectangles} />
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
       </div>
     </div>
   )
