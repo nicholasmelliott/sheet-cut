cd5ad232b5b9a04e759e4f6eb184a57689d18b50
nicholasmelliott <30277860+nicholasmelliott@users.noreply.github.com>
2023-03-19 19:18:19 -0400

    Update colors for forms and to-be-cut sheets

--- src/Utils/colorByIndex.js
+++ src/Utils/colorByIndex.js
@@ -1,16 +1,19 @@
+
 const colors = [
-    '#cc6699',
-    '#9c6', 
-    '#69c',
-    '#9933cc',
-    '#669933',
-    '#cc9966',
-    '#336699',
-    '#ff9900',
-    '#663399',
-    '#0099cc',
-  ];
-  
+  "#e51636",
+  "#36e579", // Spring Green
+  "#e5cc36", // Goldenrod
+  "#363be5", // Royal Blue
+  "#e5369c", // Deep Pink
+  "#36e5cf", // Celeste
+  "#e56236", // Dark Orange
+  "#3684e5", // Dodger Blue
+  "#9c36e5", // Purple
+  "#36b8e5", // Maya Blue
+  "#e5366b"  // Cerise
+];
+
+
   const getHexColorByIndex = (index) => {
     // Ensure index is within bounds of colors array
     const boundedIndex = index % colors.length;
