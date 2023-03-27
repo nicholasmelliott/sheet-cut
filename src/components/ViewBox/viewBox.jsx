6da057fbfdaead4f449dfa13681b9bfa64ac89bf
nicholasmelliott <30277860+nicholasmelliott@users.noreply.github.com>
2023-03-27 06:20:30 -0400

    Update printViewBox styling for greater readabilty and compactness on printed page.

--- src/components/ViewBox/viewBox.jsx
+++ src/components/ViewBox/viewBox.jsx
@@ -54,8 +54,8 @@ const ViewBoxWrapper = (props) => {
   }
 
   return (
-    <div style={{height: "100%"}}>
-      <div className="container testimonial-group"  ref={printRef} style={{height: "100%"}}>
+    <div ref={printRef} style={{height: "100%"}}>
+      <div className="container testimonial-group" style={{height: "100%"}}>
         <div className="row text-center d-flex justify-content-between p-2">
           <div className="col-6 text-success">Total Price: ${priceTotal}</div> 
           <div className="col-6 text-primary">Total Sheets: {donorBoxTotal}</div>
--- src/helpers/printViewBox.js
+++ src/helpers/printViewBox.js
@@ -5,22 +5,54 @@ const print = (printRef) => {
     <style>
       @media print {
         html, body {
-          width: 50%;
-          height: 50%;
+          width: 100%;
+          height: 100%;
           margin: 0;
           padding: 0;
         }
-        .print-content {
+        #print-content {
           width: 100%;
           height: 100%;
           margin: 0 auto;
-          display: inline-block;
+        }
+        .testimonial-group > .row {
+          display: flex;
+          flex-wrap: wrap;
+          margin: 0;
+          padding: 0;
+          height: 0;
+        }
+        .testimonial-group > .row > .col {
+          /* Set the width of each column to one-third of the display */
+          flex-basis: calc(33.33% - 20px);
+          height: calc(33.33% - 20px);
+          margin-right: 20px;
+          flex-shrink: 0;
+          padding: 0;
+        }
+        .testimonial-group > .row > .col:last-child {
+          margin-right: 0;
+        }    
+        /* Apply styling to the div containing Total Price and Total Sheets */
+        .row.text-center.d-flex.justify-content-between.p-2 {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          margin: 20px;
+        }
+        .col-6.text-success {
+          text-align: left;
+          margin-right: 20px;
+        }
+        .col-6.text-primary {
+          text-align: right;
+          margin-left: 20px;
         }
       }
     </style>
     </head>
     <body>
-      <div class="print-content">${content}</div>
+      <div id="print-content">${content}</div>
     </body>
     </html>`);
     printWindow.document.close();
