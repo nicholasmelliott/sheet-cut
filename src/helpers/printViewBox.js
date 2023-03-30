0cfce854e443192a83f483b720773b218bba3f2b
nicholasmelliott <30277860+nicholasmelliott@users.noreply.github.com>
2023-03-30 06:12:21 -0400

    Update printViewBox styling to properly display on one page when printing

--- src/helpers/printViewBox.js
+++ src/helpers/printViewBox.js
@@ -5,39 +5,31 @@ const print = (printRef) => {
     <style>
       @media print {
         html, body {
-          width: 100%;
-          height: 100%;
+          width: auto;
+          height: 800px;
           margin: 0;
           padding: 0;
         }
         #print-content {
-          width: 100%;
-          height: 100%;
-          margin: 0 auto;
+          width: auto;
+          height: auto;
+          margin: 0 auto;   
         }
         .testimonial-group > .row {
-          display: flex;
-          flex-wrap: wrap;
-          margin: 0;
-          padding: 0;
-          height: 0;
+          height: auto;
+          display: block;
         }
         .testimonial-group > .row > .col {
-          /* Set the width of each column to one-third of the display */
-          flex-basis: calc(33.33% - 20px);
-          height: calc(33.33% - 20px);
-          margin-right: 20px;
-          flex-shrink: 0;
-          padding: 0;
+          display: inline-block;
+          width: 33.33%;
+          height: 33.33%;
         }
-        .testimonial-group > .row > .col:last-child {
-          margin-right: 0;
-        }    
         /* Apply styling to the div containing Total Price and Total Sheets */
         .row.text-center.d-flex.justify-content-between.p-2 {
           display: flex;
           justify-content: center;
           align-items: center;
+          height: 0;
           margin: 20px;
         }
         .col-6.text-success {
