07873fe4feacbc89869309fdd2d81c21dafc6dc6
nicholasmelliott <30277860+nicholasmelliott@users.noreply.github.com>
2023-07-22 10:41:43 -0400

    Refactor createBoxes function for readability and extend Box class instead of modifying directly

--- src/helpers/binPacking/createBoxes.js
+++ src/helpers/binPacking/createBoxes.js
@@ -1,25 +1,50 @@
 import { BP2D as BinPacking2D } from 'binpackingjs'
 const { Box } = BinPacking2D;
 
+// Create a new class that extends the Box class
+class CustomBox extends Box {
+  constructor(width, height, w, h, thickness, index, colorIndex, quantity, wFracText, hFracText, tFracText) {
+    super(width, height);
+    this.w = w;
+    this.h = h;
+    this.thickness = thickness;
+    this.index = index;
+    this.colorIndex = colorIndex;
+    this.quantity = quantity;
+    this.wFrac = wFracText;
+    this.hFrac = hFracText;
+    this.tFrac = tFracText;
+  }
+}
+
 const createBoxes = (array) => {
     return array.map((el) => {
       const { width, height, heightFraction, widthFraction, heightFractionText, widthFractionText, thicknessFraction, thicknessFractionText, index, colorIndex, quantity } = el;
-      const updatedWidth = width > height ? height : width;
-      const updatedHeight = width > height ? width : height;
-      const updatedWFrac = width > height ? heightFraction : widthFraction;
-      const updatedHFrac = width > height ? widthFraction : heightFraction;
-      const updatedWFracText = width > height ? heightFractionText : widthFractionText;
-      const updatedHFracText = width > height ? widthFractionText : heightFractionText;
-      const box = new Box((updatedWidth + updatedWFrac), (updatedHeight + updatedHFrac));
-      box.thickness = thicknessFraction;
-      box.index = index;
-      box.colorIndex = colorIndex;
-      box.w = updatedWidth;
-      box.wFrac = updatedWFracText;
-      box.h = updatedHeight;
-      box.hFrac = updatedHFracText;
-      box.tFrac = thicknessFractionText
-      box.quantity = quantity;
+      
+      const isWidthGreater = width > height;
+      const dimensions = {
+        updatedWidth: isWidthGreater ? height : width,
+        updatedHeight: isWidthGreater ? width : height,
+        updatedWFrac: isWidthGreater ? heightFraction : widthFraction,
+        updatedHFrac: isWidthGreater ? widthFraction : heightFraction,
+        updatedWFracText: isWidthGreater ? heightFractionText : widthFractionText,
+        updatedHFracText: isWidthGreater ? widthFractionText : heightFractionText
+      };
+  
+      const box = new CustomBox(
+        dimensions.updatedWidth + dimensions.updatedWFrac, 
+        dimensions.updatedHeight + dimensions.updatedHFrac,
+        dimensions.updatedWidth,
+        dimensions.updatedHeight, 
+        thicknessFraction, 
+        index, 
+        colorIndex, 
+        quantity, 
+        dimensions.updatedWFracText, 
+        dimensions.updatedHFracText, 
+        thicknessFractionText
+      );
+  
       return box;
     });
   }
