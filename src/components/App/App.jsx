6c98421a80434e0beec575c1502d91bd28b36a8a
nicholasmelliott <30277860+nicholasmelliott@users.noreply.github.com>
2023-08-13 08:05:24 -0400

    Refactor inline styling into app css file.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -49,20 +49,20 @@ function App() {
 
   return (
     <div className="App">
-      <iframe id="ifmcontentstoprint" style={{"height": "0px", "width": "0px", "position": "absolute"}}></iframe>
-      <div style={{height: "80vh"}}>
+      <iframe id="ifmcontentstoprint" className='ifmcontentstoprint'></iframe>
+      <div className="display-wrapper">
         <Totals rectangles={rectangles} printRef={printTotalsRef} />
         <ViewBox rectangles={rectangles} printRef={printViewBoxRef} />
       </div>
-      <div style={{position: 'fixed', bottom: 0, left: 0, right: 0}}>
-        <div className="d-flex align-items-center justify-content-center w-100" style={{backgroundColor: '#eee', height: '60px', width: "100%"}}>
+      <div className="dropdown-wrapper">
+        <div className="d-flex align-items-center justify-content-center w-100 dropdown-background">
           <Dropdown className="d-flex align-items-center justify-content-between p-2 w-100" autoClose={false} drop="up" onToggle={handleToggle} defaultShow={isMenuOpen}>
-            <Button variant="primary" style={{width: "30%"}} onClick={handlePrint}>Print</Button>
-            <Dropdown.Toggle variant="danger" id="dropdown-autoclose-false dropdown-button-drop-up" className='mx-auto' style={{width: "30%"}}>
+            <Button variant="primary" className="main-nav-btn" onClick={handlePrint}>Print</Button>
+            <Dropdown.Toggle variant="danger" id="dropdown-autoclose-false dropdown-button-drop-up" className='mx-auto main-nav-btn'>
               <span>&#9776;</span>
             </Dropdown.Toggle>
-            <Button variant="secondary" style={{width: "30%"}} onClick={addRow} disabled={!isMenuOpen}>Add</Button>
-            <Dropdown.Menu className="w-100 p-3" style={{background: 'transparent', border: "0px"}}>
+            <Button variant="secondary" className="main-nav-btn" onClick={addRow} disabled={!isMenuOpen}>Add</Button>
+            <Dropdown.Menu className="w-100 p-3 dropdown-menu-background">
               <ResponsiveList rows={rows} setRows={setRows} rectangles={rectangles} setRectangles={setRectangles} materials={materials}/>
             </Dropdown.Menu>
           </Dropdown>
