f8d1320c4faff8351cfbf6fbef88d1ddf8e4eec2
nicholasmelliott <30277860+nicholasmelliott@users.noreply.github.com>
2023-08-17 22:57:28 -0400

    Add ErrorBoundary component for app wide error handling.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Button, Dropdown } from 'react-bootstrap';
 import './App.css';
+import ErrorBoundary from '../ErrorBoundary/errorBoundary'
 import ResponsiveList from '../RespList/respList';
 import Totals from '../Totals/totals';
 import ViewBox from '../ViewBox/viewBox';
@@ -48,27 +49,29 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <iframe id="ifmcontentstoprint" className='ifmcontentstoprint'></iframe>
-      <div className="display-wrapper">
-        <Totals rectangles={rectangles} printRef={printTotalsRef} />
-        <ViewBox rectangles={rectangles} printRef={printViewBoxRef} />
-      </div>
-      <div className="dropdown-wrapper">
-        <div className="d-flex align-items-center justify-content-center w-100 dropdown-background">
-          <Dropdown className="d-flex align-items-center justify-content-between p-2 w-100" autoClose={false} drop="up" onToggle={handleToggle} defaultShow={isMenuOpen}>
-            <Button variant="primary" className="main-nav-btn" onClick={handlePrint}>Print</Button>
-            <Dropdown.Toggle variant="danger" id="dropdown-autoclose-false dropdown-button-drop-up" className='mx-auto main-nav-btn'>
-              <span>&#9776;</span>
-            </Dropdown.Toggle>
-            <Button variant="secondary" className="main-nav-btn" onClick={addRow} disabled={!isMenuOpen}>Add</Button>
-            <Dropdown.Menu className="w-100 p-3 dropdown-menu-background">
-              <ResponsiveList rows={rows} setRows={setRows} rectangles={rectangles} setRectangles={setRectangles} materials={materials}/>
-            </Dropdown.Menu>
-          </Dropdown>
+    <ErrorBoundary>
+      <div className="App">
+        <iframe id="ifmcontentstoprint" className='ifmcontentstoprint'></iframe>
+        <div className="display-wrapper">
+          <Totals rectangles={rectangles} printRef={printTotalsRef} />
+          <ViewBox rectangles={rectangles} printRef={printViewBoxRef} />
+        </div>
+        <div className="dropdown-wrapper">
+          <div className="d-flex align-items-center justify-content-center w-100 dropdown-background">
+            <Dropdown className="d-flex align-items-center justify-content-between p-2 w-100" autoClose={false} drop="up" onToggle={handleToggle} defaultShow={isMenuOpen}>
+              <Button variant="primary" className="main-nav-btn" onClick={handlePrint}>Print</Button>
+              <Dropdown.Toggle variant="danger" id="dropdown-autoclose-false dropdown-button-drop-up" className='mx-auto main-nav-btn'>
+                <span>&#9776;</span>
+              </Dropdown.Toggle>
+              <Button variant="secondary" className="main-nav-btn" onClick={addRow} disabled={!isMenuOpen}>Add</Button>
+              <Dropdown.Menu className="w-100 p-3 dropdown-menu-background">
+                <ResponsiveList rows={rows} setRows={setRows} rectangles={rectangles} setRectangles={setRectangles} materials={materials}/>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
         </div>
       </div>
-    </div>
+    </ErrorBoundary>
   )
 }
 
