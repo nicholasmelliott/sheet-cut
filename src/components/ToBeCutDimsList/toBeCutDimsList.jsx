cf148687fa88a330c06181ec043f17f0cc6c7ebc
nicholasmelliott <30277860+nicholasmelliott@users.noreply.github.com>
2023-09-02 17:02:47 -0400

    Refactor unnecessary inline styling into complimentary css file.

--- src/components/ToBeCutDimsList/toBeCutDimsList.jsx
+++ src/components/ToBeCutDimsList/toBeCutDimsList.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
+import './toBeCutDimsList.css';
 
 const ToBeCutDimensionsList = ({ rectangles }) => {
-  const toBeCutMainBGIncrement = 0;
-  const toBeCutMainBGHeight = 40;
-  const toBeCutMainBGMargin = 15;
-  const toBeCutMainDimFontSize = 25;
-  const toBeCutMainDimFill = "#000";
-  const toBeCutSideDimsFill = "#FFF";
+  
   const processedColorIndexes = new Set();
   let setIndex = 0;
 
@@ -19,58 +15,28 @@ const ToBeCutDimensionsList = ({ rectangles }) => {
           processedColorIndexes.add(b.colorIndex);
           setIndex++;
           return (
-              <div key={j}>
-              <div
-                className="row"
-                style={{
-                  width: "100%",
-                  height: toBeCutMainBGHeight,
-                  backgroundColor: backgroundColor,
-                  border: "1px solid #eee",
-                  borderRadius: "10px",
-                  marginBottom: toBeCutMainBGIncrement,
-                }}
-              >
-                <div className="col-4">
-                  <span
-                    style={{
-                      fill: toBeCutMainDimFill,
-                      fontSize: toBeCutMainDimFontSize,
-                      marginLeft: toBeCutMainBGMargin,
-                      height: "10px"
-                    }}
-                  >
+            <div key={j}>
+              <div className="row main" style={{ backgroundColor: backgroundColor, margin: 0}}>
+                <div className="col-3 text-start box-index">
+                  <span>
                     #{b.index + 1}
                   </span>
                 </div>
-                <div className="col-4">
-                  <span
-                    style={{
-                      fill: toBeCutMainDimFill,
-                      fontSize: toBeCutMainDimFontSize,
-                      textAlign: "center",
-                    }}
-                  >
+                <div className="col-6 box-dims">
+                  <span>
                     {b.w !== 0 && ` ${b.w}`}
                     {b.wFrac !== "0" && b.wFrac !== "" && ` ${b.wFrac}"`} x
                     {b.h !== 0 && ` ${b.h}`}
                     {b.hFrac !== "0" && b.hFrac !== "" && ` ${b.hFrac}"`}
                   </span>
                 </div>
-                <div className="col-4">
-                  <span
-                    style={{
-                      fill: toBeCutMainDimFill,
-                      fontSize: toBeCutMainDimFontSize,
-                      textAlign: "end",
-                    }}
-                  >
+                <div className="col-3 text-end box-quantity" style={{paddingLeft: "0px"}}>
+                  <span>
                     {`${rectangles.boxQuantity[b.colorIndex]} of ${b.quantity}`}
                   </span>
                 </div>
               </div>
-            </div>
-                   
+            </div>         
           );
         }
       })}
