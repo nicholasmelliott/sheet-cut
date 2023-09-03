547670e11459de6c7e327172e5b65a347915537b
nicholasmelliott <30277860+nicholasmelliott@users.noreply.github.com>
2023-09-03 09:20:21 -0400

    Refactor spacing for readability.

--- src/components/ViewBox/viewBox.jsx
+++ src/components/ViewBox/viewBox.jsx
@@ -109,133 +109,133 @@ const ViewBoxWrapper = (props) => {
             return(
             <div key={i} className="col">
               <svg viewBox={`0 0 ${(r.width * multiplier) + cBorder} ${(r.height * multiplier) + cBorder + heightIncrement}`} width="100%" height="100%" preserveAspectRatio="none">
-              <defs>
-                <linearGradient id={`donorPieceGradient${i}`} x1="0%" y1="0%" x2="100%" y2="100%">
-                  <stop offset="0%" stopColor="#FFFFFF" stopOpacity="0.3" />
-                  <stop offset="10%" stopColor="#FFFFFF" stopOpacity="0.3" />
-                  <stop offset="40%" stopColor={donorPieceFill} stopOpacity="0.1" />
-                  <stop offset="60%" stopColor={donorPieceFill} stopOpacity="0.1" />
-                  <stop offset="90%" stopColor="#FFFFFF" stopOpacity="0.3" />
-                  <stop offset="100%" stopColor="#FFFFFF" stopOpacity="0.3" />
-                </linearGradient>
-              </defs>
-              <g key={i}>
-            {/* Draw donor piece */}
-            <rect
-              x={cMargin}
-              y={cMargin + prevHeight}
-              width={r.width * multiplier}
-              height={r.height * multiplier}
-              fill={`url(#donorPieceGradient${i})`}
-              stroke={donorPieceStroke}
-            />
-            {/* Draw donor piece dimensions next to corresponding sides */}
-            <text 
-              x={cMargin + (r.width * multiplier) / 2} 
-              y={cMargin - scaleWithWindow(r.width, donorTopDimDecrement) + prevHeight} 
-              textAnchor="middle" 
-              fontSize={scaleWithWindow(r.width, donorSidesFontSize)}
-              fill={donorSidesTextFill}
-            >
-              {r.width}
-            </text>
-            <text 
-              x={cMargin - scaleWithWindow(r.width, donorLeftDimDecrement)} 
-              y={cMargin + (r.height * multiplier) / 2 + prevHeight} 
-              textAnchor="middle" 
-              fontSize={scaleWithWindow(r.width, donorSidesFontSize)}
-              fill={donorSidesTextFill}
-            >
-              {r.height}
-            </text>
-            <text 
-              x={cMargin + (r.width * multiplier) / 2} 
-              y={(cMargin + scaleWithWindow(r.width, donorBottomPriceIncrement)) + (r.height * multiplier)} 
-              textAnchor="middle" 
-              fontSize={scaleWithWindow(r.width, donorPriceFontSize)} 
-              fill={donorPriceTextFill}
-            >
-              ${r.price} | {r.thicknessText}
-            </text>
-            {/* Draw piece to-be-cut inside donor piece */}
-            {r.boxes.map((b, j) => (
-              <g key={j}>
                 <defs>
-                  <linearGradient id={`toBeCutPieceGradient${i}${j}`} x1="0%" y1="0%" x2="100%" y2="100%">
-                    <stop offset="0%" stopColor="#FFFFFF" stopOpacity="0.9" />
-                    <stop offset="10%" stopColor="#FFFFFF" stopOpacity="0.9" />
-                    <stop offset="40%" stopColor={getHexColorByIndex(b.colorIndex)} stopOpacity="1" />
-                    <stop offset="60%" stopColor={getHexColorByIndex(b.colorIndex)} stopOpacity="1" />
-                    <stop offset="90%" stopColor="#FFFFFF" stopOpacity="0.9" />
-                    <stop offset="100%" stopColor="#FFFFFF" stopOpacity="0.9" />
+                  <linearGradient id={`donorPieceGradient${i}`} x1="0%" y1="0%" x2="100%" y2="100%">
+                    <stop offset="0%" stopColor="#FFFFFF" stopOpacity="0.3" />
+                    <stop offset="10%" stopColor="#FFFFFF" stopOpacity="0.3" />
+                    <stop offset="40%" stopColor={donorPieceFill} stopOpacity="0.1" />
+                    <stop offset="60%" stopColor={donorPieceFill} stopOpacity="0.1" />
+                    <stop offset="90%" stopColor="#FFFFFF" stopOpacity="0.3" />
+                    <stop offset="100%" stopColor="#FFFFFF" stopOpacity="0.3" />
                   </linearGradient>
                 </defs>
-                <rect
-                  key={j}
-                  x={b.x * multiplier + cMargin}
-                  y={b.y * multiplier + cMargin + prevHeight}
-                  width={b.width * multiplier}
-                  height={b.height * multiplier}
-                  fill={`url(#toBeCutPieceGradient${i}${j})`}
-                  strokeDasharray={scaleWithWindow(r.width, toBeCutPieceStrokeDasharray)}
-                  stroke={toBeCutPieceStroke}
-                />
-                {/* Draw index of the piece to-be-cut in center of rect */}
-                <text
-                  x={(b.x * multiplier + cMargin) + (b.width * multiplier / 2)}
-                  y={(b.y * multiplier + cMargin + prevHeight) + (b.height * multiplier / 2)}
-                  fill={toBeCutMainDimFill}
-                  textAnchor="middle"
-                  fontSize={scaleWithWindow(r.width, toBeCutSheetNumFontSize)}
-                  dominantBaseline="middle"
-                >
-                  {renderIndex(b, renderIndexMinSizeWH)}
-                </text>
-                {/* Draw the top dimension */}
-                <text
-                  x={(b.x * multiplier + cMargin) + (b.width * multiplier / 2)}
-                  y={(b.y * multiplier + cMargin + scaleWithWindow(r.width, toBeCutTopDimIncrement) + prevHeight)}
-                  fill={toBeCutSideDimsFill}
-                  textAnchor="middle"
-                  fontSize={scaleWithWindow(r.width, toBeCutSideDimsFontSize)}
-                >
-                  {renderWidthDim(b, minHeightSize)}
-                </text>
-                {/* Draw the bottom dimension */}
-                <text
-                  x={(b.x * multiplier + cMargin) + (b.width * multiplier / 2)}
-                  y={(b.y * multiplier + cMargin + b.height * multiplier - scaleWithWindow(r.width, toBeCutBottomDimDecrement) + prevHeight)}
-                  fill={toBeCutSideDimsFill}
-                  textAnchor="middle"
-                  fontSize={scaleWithWindow(r.width, toBeCutSideDimsFontSize)}
-                >
-                  {renderWidthDim(b, minHeightSize)}
-                </text>
-                {/* Draw the left dimension */}
-                <text
-                  x={(b.x * multiplier + cMargin + scaleWithWindow(r.width, toBeCutLeftDimIncrement))}
-                  y={(b.y * multiplier + cMargin + (b.height * multiplier / 2) + prevHeight)}
-                  fill={toBeCutSideDimsFill}
-                  textAnchor="start"
-                  fontSize={scaleWithWindow(r.width, toBeCutSideDimsFontSize)}
-                  dominantBaseline="middle"
-                >
-                  {renderHeightDim(b, minWidthSize)}
-                </text>
-                {/* Draw the right dimension */}
-                <text
-                  x={(b.x * multiplier + cMargin + b.width * multiplier - scaleWithWindow(r.width, toBeCutRightDimDecrement))}
-                  y={(b.y * multiplier + cMargin + (b.height * multiplier / 2) + prevHeight)}
-                  fill={toBeCutSideDimsFill}
-                  textAnchor="end"
-                  fontSize={scaleWithWindow(r.width, toBeCutSideDimsFontSize)}
-                  dominantBaseline="middle"
-                >
-                  {renderHeightDim(b, minWidthSize)}
-                </text>
-              </g>
-            ))}
-          </g>
+                <g key={i}>
+                  {/* Draw donor piece */}
+                  <rect
+                    x={cMargin}
+                    y={cMargin + prevHeight}
+                    width={r.width * multiplier}
+                    height={r.height * multiplier}
+                    fill={`url(#donorPieceGradient${i})`}
+                    stroke={donorPieceStroke}
+                  />
+                  {/* Draw donor piece dimensions next to corresponding sides */}
+                  <text 
+                    x={cMargin + (r.width * multiplier) / 2} 
+                    y={cMargin - scaleWithWindow(r.width, donorTopDimDecrement) + prevHeight} 
+                    textAnchor="middle" 
+                    fontSize={scaleWithWindow(r.width, donorSidesFontSize)}
+                    fill={donorSidesTextFill}
+                  >
+                    {r.width}
+                  </text>
+                  <text 
+                    x={cMargin - scaleWithWindow(r.width, donorLeftDimDecrement)} 
+                    y={cMargin + (r.height * multiplier) / 2 + prevHeight} 
+                    textAnchor="middle" 
+                    fontSize={scaleWithWindow(r.width, donorSidesFontSize)}
+                    fill={donorSidesTextFill}
+                  >
+                    {r.height}
+                  </text>
+                  <text 
+                    x={cMargin + (r.width * multiplier) / 2} 
+                    y={(cMargin + scaleWithWindow(r.width, donorBottomPriceIncrement)) + (r.height * multiplier)} 
+                    textAnchor="middle" 
+                    fontSize={scaleWithWindow(r.width, donorPriceFontSize)} 
+                    fill={donorPriceTextFill}
+                  >
+                    ${r.price} | {r.thicknessText}
+                  </text>
+                  {/* Draw piece to-be-cut inside donor piece */}
+                  {r.boxes.map((b, j) => (
+                    <g key={j}>
+                      <defs>
+                        <linearGradient id={`toBeCutPieceGradient${i}${j}`} x1="0%" y1="0%" x2="100%" y2="100%">
+                          <stop offset="0%" stopColor="#FFFFFF" stopOpacity="0.9" />
+                          <stop offset="10%" stopColor="#FFFFFF" stopOpacity="0.9" />
+                          <stop offset="40%" stopColor={getHexColorByIndex(b.colorIndex)} stopOpacity="1" />
+                          <stop offset="60%" stopColor={getHexColorByIndex(b.colorIndex)} stopOpacity="1" />
+                          <stop offset="90%" stopColor="#FFFFFF" stopOpacity="0.9" />
+                          <stop offset="100%" stopColor="#FFFFFF" stopOpacity="0.9" />
+                        </linearGradient>
+                      </defs>
+                      <rect
+                        key={j}
+                        x={b.x * multiplier + cMargin}
+                        y={b.y * multiplier + cMargin + prevHeight}
+                        width={b.width * multiplier}
+                        height={b.height * multiplier}
+                        fill={`url(#toBeCutPieceGradient${i}${j})`}
+                        strokeDasharray={scaleWithWindow(r.width, toBeCutPieceStrokeDasharray)}
+                        stroke={toBeCutPieceStroke}
+                      />
+                      {/* Draw index of the piece to-be-cut in center of rect */}
+                      <text
+                        x={(b.x * multiplier + cMargin) + (b.width * multiplier / 2)}
+                        y={(b.y * multiplier + cMargin + prevHeight) + (b.height * multiplier / 2)}
+                        fill={toBeCutMainDimFill}
+                        textAnchor="middle"
+                        fontSize={scaleWithWindow(r.width, toBeCutSheetNumFontSize)}
+                        dominantBaseline="middle"
+                      >
+                        {renderIndex(b, renderIndexMinSizeWH)}
+                      </text>
+                      {/* Draw the top dimension */}
+                      <text
+                        x={(b.x * multiplier + cMargin) + (b.width * multiplier / 2)}
+                        y={(b.y * multiplier + cMargin + scaleWithWindow(r.width, toBeCutTopDimIncrement) + prevHeight)}
+                        fill={toBeCutSideDimsFill}
+                        textAnchor="middle"
+                        fontSize={scaleWithWindow(r.width, toBeCutSideDimsFontSize)}
+                      >
+                        {renderWidthDim(b, minHeightSize)}
+                      </text>
+                      {/* Draw the bottom dimension */}
+                      <text
+                        x={(b.x * multiplier + cMargin) + (b.width * multiplier / 2)}
+                        y={(b.y * multiplier + cMargin + b.height * multiplier - scaleWithWindow(r.width, toBeCutBottomDimDecrement) + prevHeight)}
+                        fill={toBeCutSideDimsFill}
+                        textAnchor="middle"
+                        fontSize={scaleWithWindow(r.width, toBeCutSideDimsFontSize)}
+                      >
+                        {renderWidthDim(b, minHeightSize)}
+                      </text>
+                      {/* Draw the left dimension */}
+                      <text
+                        x={(b.x * multiplier + cMargin + scaleWithWindow(r.width, toBeCutLeftDimIncrement))}
+                        y={(b.y * multiplier + cMargin + (b.height * multiplier / 2) + prevHeight)}
+                        fill={toBeCutSideDimsFill}
+                        textAnchor="start"
+                        fontSize={scaleWithWindow(r.width, toBeCutSideDimsFontSize)}
+                        dominantBaseline="middle"
+                      >
+                        {renderHeightDim(b, minWidthSize)}
+                      </text>
+                      {/* Draw the right dimension */}
+                      <text
+                        x={(b.x * multiplier + cMargin + b.width * multiplier - scaleWithWindow(r.width, toBeCutRightDimDecrement))}
+                        y={(b.y * multiplier + cMargin + (b.height * multiplier / 2) + prevHeight)}
+                        fill={toBeCutSideDimsFill}
+                        textAnchor="end"
+                        fontSize={scaleWithWindow(r.width, toBeCutSideDimsFontSize)}
+                        dominantBaseline="middle"
+                      >
+                        {renderHeightDim(b, minWidthSize)}
+                      </text>
+                    </g>
+                  ))}
+                </g>
               </svg>
               <ToBeCutDimsList rectangles={r} />
             </div>
