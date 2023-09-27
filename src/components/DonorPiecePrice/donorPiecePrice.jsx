4e5509c162b8f22beb62c2d1d70f648dc761d7c9
nicholasmelliott <30277860+nicholasmelliott@users.noreply.github.com>
2023-09-26 21:40:12 -0400

    Update text color for greater readability and clarity.

--- src/components/DonorPiecePrice/donorPiecePrice.jsx
+++ src/components/DonorPiecePrice/donorPiecePrice.jsx
@@ -5,8 +5,8 @@ const DonorPiecePrice = ({ rectangles }) => {
   return (
     <div>
         <div className="row text-center d-flex justify-content-between p-2">
-          <div className="col-6 text-success">${rectangles.price}</div>
-          <div className="col-6 text-primary">{rectangles.thicknessText}</div>
+          <div className="col-6">${rectangles.price}</div>
+          <div className="col-6">{rectangles.thicknessText}</div>
         </div>
     </div>
   );
