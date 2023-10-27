112be4174189d1de7b961dc670dc957254f787ad
nicholasmelliott <30277860+nicholasmelliott@users.noreply.github.com>
2023-10-27 06:02:24 -0400

    Optimize areDimensionsValid function by preprocessing thickness values for faster comparisons.

--- src/helpers/validateDimensions.js
+++ src/helpers/validateDimensions.js
@@ -1,8 +1,22 @@
 import dataArray from '../data/Quantity_10_ProductResults_2023-03-18_08-44-35-AM_ProductResults_2023-02-25_08-30-06-AM_product_w_Dimensions.json';
-  
+
+// Preprocess the thickness values to rounded numbers 
+const preProcessedDataArray = dataArray.map(dataObject => {
+    if (dataObject.dimensions && !isNaN(dataObject.dimensions.thickness)) {
+      return {
+        ...dataObject,
+        dimensions: {
+          ...dataObject.dimensions,
+          thickness: parseFloat(parseFloat(dataObject.dimensions.thickness).toFixed(1))
+        }
+      };
+    }
+    return dataObject;
+  });
+
 // Validate if user entered dimensions fall within available dimensions
 const areDimensionsValid = (userDimensionsArray) => {
-    if (!Array.isArray(userDimensionsArray) || !Array.isArray(dataArray)) {
+    if (!Array.isArray(userDimensionsArray) || !Array.isArray(preProcessedDataArray)) {
       return false;
     }
   
@@ -10,24 +24,26 @@ const areDimensionsValid = (userDimensionsArray) => {
     for (const userDimensions of userDimensionsArray) {
       const userWidth = (parseFloat(userDimensions.width) || 0) + (parseFloat(userDimensions.widthFraction) || 0);
       const userHeight = (parseFloat(userDimensions.height) || 0) + (parseFloat(userDimensions.heightFraction) || 0);
+      const userThickness = parseFloat(userDimensions.thicknessFraction);
   
       let isValidForAnyDataObject = false;
   
-      // Check against each dataObject in the dataArray
-      for (const dataObject of dataArray) {
+      // Check against each dataObject in the preProcessedDataArray
+      for (const dataObject of preProcessedDataArray) {
         const { dimensions: dataDimensions } = dataObject;
         
         if (!dataDimensions) {
           continue;
         }
   
-        const { width: dataWidth, height: dataHeight } = dataDimensions;
+        const { width: dataWidth, height: dataHeight, thickness: dataThickness } = dataDimensions;
   
         if (
           typeof userWidth === 'number' && typeof userHeight === 'number' &&
-          typeof dataWidth === 'number' && typeof dataHeight === 'number'
+          typeof dataWidth === 'number' && typeof dataHeight === 'number' &&
+          typeof dataThickness === 'number' && typeof userThickness === 'number'
         ) {
-          if ((userWidth <= dataWidth && userHeight <= dataHeight) || (userWidth <= dataHeight && userHeight <= dataWidth)) {
+          if ((userWidth <= dataWidth && userHeight <= dataHeight && userThickness === dataThickness) || (userWidth <= dataHeight && userHeight <= dataWidth && userThickness === dataThickness)) {
             isValidForAnyDataObject = true;
             break;
           }
